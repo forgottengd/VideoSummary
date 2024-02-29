b01b0dc0f7dedeb9cde2d259b6f158afc15c70e5
forgottengd <konsts@yandex.ru>
2024-02-29 17:36:19 +0300

    Introduces working with local llm, some fixes

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import os
 import re
 import streamlit as st
 import configparser
+import src.local_llm as local_llm
 from uuid import uuid4
 from src.utils import convert_mp4_to_mp3, download_audio, video_title, summarize_openai_text, transcribe
+from dotenv import load_dotenv
 
 
 # Function to save configuration to file
@@ -17,11 +19,14 @@ def main():
     config = configparser.ConfigParser()
     config.read("config.ini")
 
+    # Load .env
+    load_dotenv()
+
     # Setup streamlit
     st.set_page_config(layout="wide")
     st.title("Видео Суммаризатор")
     col1, col2 = st.columns([3, 7], gap='medium')
-    summary = ""
+    summary = v_title = ""
 
     with col1:
         # Voice transcribe model selector
@@ -41,12 +46,21 @@ def main():
                                                    ("gpt-3.5-turbo", "gpt-4", "gpt-4-turbo-preview"),
                                                    key='openai_model_select',
                                                    label_visibility='collapsed')
-                key = config.get("Settings", "OPEN_AI_KEY", fallback="")
-                openai_api_key = st.text_input("Введите API-ключ для доступа к OpenAI API", value=key)
+                openai_api_key = os.getenv("OPEN_AI_KEY") or ''
+                # If no API Key in .env file, create text input field and ask user to enter key there
+                if openai_api_key == '':
+                    openai_api_key_input = st.text_input("Введите API-ключ для доступа к OpenAI API",
+                                                         help="If you don't want to enter OpenAI API key each "
+                                                              "time, you can write it into .env "
+                                                              "file in root directory like this: "
+                                                              "OPEN_AI_KEY = 'your_key'",
+                                                         key='openai_api_key_input')
             # Chose local llm
             else:
-                llm_select = st.selectbox("Выберите модель для суммаризации текста", ("local1", "local2"),
-                                          key='llm_select')
+                if not hasattr(local_llm, 'summarize_local'):
+                    st.error("Необходимо импортировать в проект локальные LLM модели, образец "
+                             "находится в файле local_llm.py")
+                    st.stop()
 
         # Paste url to youtube video
         youtube_url = st.text_input("Вставьте ссылку на видеоролик в youtube:")
@@ -55,14 +69,16 @@ def main():
         if re.match(r"^https://www.youtube.com/watch\?v=[a-zA-Z0-9_-]*$", youtube_url):
             # if user chose summirize with OpenAI check for API key
             if summarize_checkbox and summary_method_select == "OpenAI API":
-                if openai_api_key == "":
-                    st.error("Необходимо ввести ключ OpenAI API")
-                    st.stop()
+                if openai_api_key == '':
+                    if openai_api_key_input == "":
+                        st.error("Необходимо ввести ключ OpenAI API")
+                        st.stop()
+                    else:
+                        openai_api_key = openai_api_key_input
             # Show video title
-            title_placeholder = st.subheader(f"Название видео: {video_title(youtube_url)}")
-            # Display video
-            # st.video(youtube_url)
+            v_title = video_title(youtube_url)
 
+            # create 'Analyze' button
             transcribe_button = st.empty()
             progress_placeholder = st.empty()
 
@@ -88,7 +104,6 @@ def main():
                     print(e)
                     st.error("Пожалуйста, предоставьте корректную ссылку на видео!")
                     transcribe_button.empty()
-                    title_placeholder.empty()
                     progress_placeholder.empty()
                     st.stop()
 
@@ -97,10 +112,10 @@ def main():
                     progress_placeholder.empty()
                     with st.spinner("Распознавание аудио..."):
                         summary = transcribe(convert_path, model_name=whisper_model_select)
+                        print("Transcribe is done")
                 except Exception as e:
                     print(e)
                     st.error("Ошибка распознавания. Пожалуйста, попробуйте еще раз!")
-                    title_placeholder.empty()
                     st.stop()
 
                 if summarize_checkbox:
@@ -110,25 +125,25 @@ def main():
                         progress_placeholder.text("Суммаризация...")
                         if summary_method_select == "OpenAI API":
                             summary = summarize_openai_text(summary, openai_model_select, openai_api_key)
-                            config.set("Settings", "whisper", whisper_model_select)
                             config.set("Settings", "openai_model", openai_model_select)
-                            config.set("Settings", "OPEN_AI_KEY", openai_api_key)
-                            save_config(config)
                         else:
-                            # TODO: find some models
-                            summary = "Method not working"
+                            summary = local_llm.summarize_local(summary)
+                        # Save settings
+                        config.set("Settings", "whisper", whisper_model_select)
+                        config.set("Settings", "summary_method", summary_method_select)
+                        save_config(config)
                     except Exception as e:
                         st.error(f"Ошибка суммаризации. Пожалуйста, попробуйте еще раз!\nТекст ошибки: {e}")
-                        title_placeholder.empty()
                         progress_placeholder.empty()
                         st.stop()
                 progress_placeholder.empty()
 
     with col2:
+        title_placeholder = st.empty() if v_title == "" else st.subheader(f"Название видео: {v_title}")
         summ_text = st.text_area(label="Результат", value=summary, height=500)
 
 
 if __name__ == "__main__":
     main()
 
-# https://www.youtube.com/watch?v=XxCZC5dF8D8
+# https://www.youtube.com/watch?v=WGD8qvsZ1jM
---
+++ src/local_llm.py
@@ -0,0 +1,33 @@
+# This file contains code for run local LLM
+from src.utils import summary_prompt
+
+# # Example with GPT4All
+# from gpt4all import GPT4All
+#
+#
+# def summarize_local(input_text: str) -> str:
+#     model = GPT4All("mistral-7b-instruct-v0.1.Q4_0.gguf", device="gpu")
+#     output = model.generate(summary_prompt(input_text), max_tokens=1024, temp=0.6)
+#     return output
+
+# Example with HuggingFace
+# import os
+# from transformers import AutoTokenizer, AutoModelForCausalLM
+#
+#
+# def summarize_local(input_text: str) -> str:
+#     # load token for HuggingFace
+#     access_token = os.getenv("huggingface_token")
+#
+#     # load tokenizer and model
+#     tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it", token=access_token)
+#     model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it", token=access_token)
+#
+#     # tokenize text
+#     input_ids = tokenizer(summary_prompt(input_text), return_tensors="pt")
+#
+#     # generate answer
+#     outputs = model.generate(**input_ids, max_length=1024)
+#
+#     # return decoded output
+#     return tokenizer.decode(outputs[0])
